766a616ea15c950bc2bd1a3460e78b1cd84aa378
yejialiang <1476707245@qq.com>
2018-11-22 09:26:04 +0800

    correct

---
+++ Module01/src/com/idea/bean/User.java
@@ -0,0 +1,11 @@
+package com.idea.bean;
+
+/**
+ * @author yejialiang
+ * @create 2018-11-22 9:23
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
